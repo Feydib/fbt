dcae5184190616393e6bf156c453fee2426497c2
NPE <npenloup@gmail.com>
2014-05-21 20:55:56 +0200

    Security policy correct

--- App/Conf/security.php
+++ App/Conf/security.php
@@ -14,7 +14,7 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
         'secured' => array(
             'pattern'   => '^/.*$',
             'anonymous' => true,
-            'form' => array('login_path' => '/login/login', 'check_path' => '/connexion','default_target_path'=>
+            'form' => array('login_path' => '/', 'check_path' => '/connexion','default_target_path'=>
 '/','always_use_default_target_path'=>true, 'use_referer' => true),
             'logout' => array('logout_path' => '/deconnexion'), // url à appeler pour se déconnecter
             'users' => $app->share(function() use ($app) {
@@ -28,7 +28,7 @@ $app['security.access_rules'] = array(
     array('^.*/admin.*$', 'ROLE_ADMIN'),
     array('^/login.*$', 'IS_AUTHENTICATED_ANONYMOUSLY'),
     array('^/$', 'IS_AUTHENTICATED_ANONYMOUSLY'),
-    array('^/.*$', 'IS_AUTHENTICATED_ANONYMOUSLY'),
+    array('^/.*$', 'ROLE_ADMIN'),
     
 );
 $app['security.role_hierarchy'] = array(
