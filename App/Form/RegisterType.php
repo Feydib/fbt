e21f1d82e39e3a6ae12214081f4e5e29d68cacd6
NPE <npenloup@gmail.com>
2014-05-31 14:40:31 +0200

    bug fix for login page

--- App/Form/RegisterType.php
+++ App/Form/RegisterType.php
@@ -12,29 +12,31 @@ class RegisterType extends AbstractType {
         $builder->add('username', "text", array("constraints" => array(
             new Assert\NotBlank(),
             new Assert\Length(array('min' => 3))),
-            "attr" => array("placeholder" => "name"))
+            "attr" => array("placeholder" => "pseudo", "class" => "form-control"))
         );
         $builder->add('email', "email", array("constraints" => array(
             new Assert\NotBlank(),
             new Assert\Email()),
-            "attr" => array("placeholder" => "email"))
+            "attr" => array("placeholder" => "email", "class" => "form-control"))
         );
         $builder->add('lastname', "text", array("constraints" => array(
             new Assert\NotBlank(),
-            new Assert\Length(array('min' => 2))))
+            new Assert\Length(array('min' => 2))),
+            "attr" => array("placeholder" => "nom", "class" => "form-control"))
         );
         $builder->add('firstname', "text", array("constraints" => array(
             new Assert\NotBlank(),
-            new Assert\Length(array('min' => 2))))
+            new Assert\Length(array('min' => 2))),
+            "attr" => array("placeholder" => "prénom", "class" => "form-control"))
         );
         $builder->add('password_repeated', 'repeated', array(
             'type' => 'password',
             'invalid_message' => 'The password fields must match.',
-            'options' => array('required' => true),
+            'options' => array('required' => true, "attr" => array("placeholder" => "Password", "class" => "form-control")),
             'first_options' => array('label' => 'Password'),
-            'second_options' => array('label' => 'Repeat Password'),
+            'second_options' => array('label' => 'Repeat Password')
+            
         ));
-        //$builder->add("agreement", 'checkbox', array('label' => "I agree with the condition of use"));
     }
 
     public function getName() {
